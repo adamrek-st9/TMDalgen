018d3baa1ea36d28be4834f4a37784a2dd5fff12
Adam <Adam>
2025-01-09 15:21:29 +0100

    male zmiany w input i main

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-#program ewolujący zadaną ilość pokoleń na podstawie początkowej populacji z pliku .traj
-
+from functions.gen_random_pop import gen_random_pop
 from functions.prep_generation import prep_generation
 from functions.calculator import get_calc
 from functions.load_config import load_config
+
 
 #wczytanie parametrow algorytmu
 config = load_config('input.txt')
 
 n_generations = config['n_generations']
-pop_filename = config['pop_filename']
 pop_size = config['pop_size']
 n_best = config['n_best']
 n_child = config['n_child']
@@ -24,13 +23,11 @@ label = config['label']
 #ustawienie kalkulatora wykorzystywanego do obliczen
 calc = get_calc(label)
 
-#przygotowanie pierwszego pokolenia na podstawie populacji poczatkowej wczytanej z podanego pliku
-prep_generation(pop_filename, pop_size, n_best, n_child, n_mut,
-                struct_filename, size, n_atoms, n_change, atom_symbol,
-                calc, mag_moment, label, 'pop1')
+#wygenerowanie populacji poczatkowej
+gen_random_pop(pop_size, struct_filename, size, n_atoms, atom_symbol, calc, mag_moment, label, 'pop0')
 
 #przygotowanie kolejnych pokolen
 for i in range(n_generations-1):
-    prep_generation(f'sorted_pop{i+1}.traj', pop_size, n_best, n_child, n_mut,
+    prep_generation(f'sorted_pop{i}.traj', pop_size, n_best, n_child, n_mut,
                     struct_filename, size, n_atoms, n_change, atom_symbol,
-                    calc, mag_moment, label, f'pop{i+2}')
+                    calc, mag_moment, label, f'pop{i+1}')
